18b829896daafa57691b4b72024b698e325acd75
안다비 안다비 <dksekqkr1234@naver.com>
2023-04-14 20:30:18 +0900

    소수인지 판별하는 기능

---
+++ git_2/IsPrime.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_2
+{
+	internal class IsPrime
+	{
+		static bool _IsPrime(int n)
+		{
+			bool IsPrime = true;
+
+			for (int i = 2; i < n; ++i)
+			{
+				if (n % i == 0)
+				{
+					IsPrime = false;
+					break;
+				}
+			}
+
+			return IsPrime;
+		}
+
+	}
+}
