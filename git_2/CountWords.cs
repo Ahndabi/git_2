532f6478d3a03398b329b44f3b2c9ed39d9f4520
안다비 안다비 <dksekqkr1234@naver.com>
2023-04-14 20:28:22 +0900

    문자열의 단어 갯수 출력 기능

---
+++ git_2/CountWords.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_2
+{
+	internal class CountWords
+	{
+		static void Main()
+		{
+			string str = Console.ReadLine();
+			string[] str2 = str.Split();
+			int a = str2.GetLength(0);
+			Console.WriteLine(a);
+		}
+
+	}
+}
