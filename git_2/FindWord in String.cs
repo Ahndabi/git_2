74375bf89aacab849ab9b2f621859cbedaafc274
안다비 안다비 <dksekqkr1234@naver.com>
2023-04-14 20:27:00 +0900

    문자열 속에 문자찾기 기능

---
+++ git_2/FindWord in String.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_2
+{
+	internal class FindWord_in_String
+	{
+		static void Main()
+		{
+			string str = "pineapple is yummy";
+			Console.WriteLine(str.IndexOf("apple"));
+		}
+
+	}
+}
